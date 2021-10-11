bec1d6d78f4d387a4f75a31957e1c0a36e71de96
Pawan <pawan_kumarp@crgl-thirdparty.com>
2021-10-11 11:05:43 +0530

    made app for POTRAIT mode
    made app for POTRAIT mode

--- app/src/main/java/cargill/com/purina/PurinaApplication.kt
+++ app/src/main/java/cargill/com/purina/PurinaApplication.kt
@@ -1,5 +1,42 @@
 package cargill.com.purina
 
+import android.app.Activity
 import android.app.Application
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+
 class PurinaApplication: Application() {
+  override fun onCreate() {
+    super.onCreate()
+    registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks{
+      override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+        activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+      }
+
+      override fun onActivityStarted(activity: Activity) {
+        TODO("Not yet implemented")
+      }
+
+      override fun onActivityResumed(activity: Activity) {
+        TODO("Not yet implemented")
+      }
+
+      override fun onActivityPaused(activity: Activity) {
+        TODO("Not yet implemented")
+      }
+
+      override fun onActivityStopped(activity: Activity) {
+        TODO("Not yet implemented")
+      }
+
+      override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+        TODO("Not yet implemented")
+      }
+
+      override fun onActivityDestroyed(activity: Activity) {
+        TODO("Not yet implemented")
+      }
+
+    })
+  }
 }
