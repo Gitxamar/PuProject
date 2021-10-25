572d69fdec66068d8b772c365de87f7c5783e1d1
arunagirigk <arunagiri_gk@crgl-thirdparty.com>
2021-10-25 16:56:00 +0530

    Contact Us Text alignments.

--- app/src/main/java/cargill/com/purina/utils/Constants.kt
+++ app/src/main/java/cargill/com/purina/utils/Constants.kt
@@ -10,8 +10,8 @@ import kotlin.properties.Delegates
 class Constants {
   companion object {
     const val DEBUG_API_KEY = "AIzaSyCJhCfsZ1Hu2TgqUMeDrQKnfcpXounMCp0"
-    //const val DEV_BASE_URL = "https://apipurina.dev.dev-cglcloud.com"
-    const val DEV_BASE_URL = "https://apipurina.stage.cglcloud.in"
+    const val DEV_BASE_URL = "https://apipurina.dev.dev-cglcloud.com"
+    //const val DEV_BASE_URL = "https://apipurina.stage.cglcloud.in"
     const val USER_LANGUAGE_CODE: String = "my_language"
     const val USER_LANGUAGE: String = "my_lang"
     const val USER_ANIMAL: String = "animal_selected"
